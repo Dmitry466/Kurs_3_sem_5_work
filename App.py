87124699767a6097d61e884dd0ff2ee1342a7d37
Dmitry466 <dima46saraev@gmail.com>
2024-12-22 21:17:56 +0300

    First version

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ def gfg():
         wg=""
         one = request.form.get("1")
         if one is not None:
-            wg="aga"
+            wg="ag"
         text = request.form.get("Text")
         return render_template("index1.html",data="cipher"+text+wg)
     return render_template("index1.html",data="")
