d5d35be8c33117ba200c2fa88d2ef339f8753c09
Dmitry466 <dima46saraev@gmail.com>
2025-01-09 20:36:28 +0300

    test

--- App.py
+++ App.py
@@ -1,5 +1,8 @@
 # importing Flask and other modules
-from flask import Flask, request, render_template
+from transliterate import translit
+from flask import Flask, request, render_template, Response
+
+from Crypton import encrypt, decrypt
 
 # Flask constructor
 app = Flask(__name__)
@@ -7,22 +10,52 @@ app = Flask(__name__)
 
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods=["GET", "POST"])
-def gfg():
+@app.route('/')
+def home():
+    return render_template("index1.html")
+
+
+@app.route('/convert_text', methods=["POST"])
+def convert_text():
     if request.method == "POST":
-        wg=""
-        one = request.form.get("1")
-        if one is not None:
-            wg="ag"
+        input_text = ""
+        encode_button = request.form.get("Encode")
+        decode_button = request.form.get("Decode")
         text = request.form.get("Text")
-        return render_template("index1.html",data="cipher"+text+wg)
-    return render_template("index1.html",data="")
-@app.route('/success', methods = ['POST'])
-def success():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(f.filename)
-        return render_template("Acknowledgement.html", name = f.filename)
+        key = request.form.get("Text1")
+        if encode_button is not None:
+            input_text = encrypt(text, key)
+        if decode_button is not None:
+            input_text = decrypt(text, key)
+
+        return render_template("index1.html", data=input_text, data1=key)
+    return render_template("index1.html", data="", data1="")
+
+
+@app.route('/convert_file', methods=["POST"])
+def convert_file():
+    file = request.files['file']
+    key = request.form.get("Text2")
+    if file:
+        file_content = file.read()
+        name_of_file = translit(file.filename, "ru", reversed=True)
+        str_file_content = str(file_content)
+        str_file_content = str_file_content[2:len(str(str_file_content)) - 1].encode('ascii').decode(
+            'unicode_escape').encode('latin-1').decode('utf-8')
+        encode_button = request.form.get("Encode")
+        decode_button = request.form.get("Decode")
+        if encode_button is not None:
+            return Response(
+                encrypt(str_file_content, key),
+                mimetype='text/plain',
+                headers={'Content-disposition': f"attachment; filename={"Encryted_" + name_of_file}"})
+        if decode_button is not None:
+            return Response(
+                decrypt(str_file_content, key),
+                mimetype='text/plain',
+                headers={'Content-disposition': f"attachment; filename={"Decrypted_" + name_of_file}"})
+    return render_template("index1.html", data2=key)
+
 
 if __name__ == '__main__':
     app.run()
